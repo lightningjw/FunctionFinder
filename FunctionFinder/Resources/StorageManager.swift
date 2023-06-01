b554e200d88bc0577abbe846c4586810d5c0f844
juswon <juswon@umich.edu>
2023-06-01 14:43:36 -0700

    Settings Page started

--- FunctionFinder/Resources/StorageManager.swift
+++ FunctionFinder/Resources/StorageManager.swift
@@ -11,6 +11,35 @@ public class StorageManager {
     
     static let shared = StorageManager()
     
+    private let bucket = Storage.storage().reference()
+    
+    public enum StorageManagerError: Error {
+        case failedToDownload
+    }
+    
     // MARK: - Public
+    
+    public func uploadUserPhotoPost(model: PhotoPost, completion: @escaping (Result<URL, Error>) -> Void) {
+        
+    }
+    
+    public func downloadImage(with reference: String, completion: @escaping (Result<URL, StorageManagerError>) -> Void) {
+        bucket.child(reference).downloadURL(completion: { url, error in
+            guard let url = url, error == nil else {
+                completion(.failure(.failedToDownload))
+                return
+            }
+            
+            completion(.success(url))
+        })
+    }
 
+}
+
+public enum UserPostType {
+    case photo, video
+}
+
+public struct PhotoPost {
+    let postType: UserPostType
 }
